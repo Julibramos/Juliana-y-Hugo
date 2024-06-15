7a3e8657e7b96cb64ee7e771567ce971c5fa8600
Julibramos <julianabularamos@gmail.com>
2024-06-15 16:28:33 -0500

    suma de digitos

--- Ejercicios Juliana/Ejercicios.py
+++ Ejercicios Juliana/Ejercicios.py
@@ -1,1 +1,12 @@
-print("Hola Juliana")
+#Ejercicio 8- suma de dígitos
+digitos = input("Ingresar numero: ")
+sum = 0
+
+for numero in digitos:
+    sum += int(numero)
+print(sum) 
+
+if sum % 2 == 0:
+    print ("La suma es par") 
+else:
+    print ("La suma es impar")
